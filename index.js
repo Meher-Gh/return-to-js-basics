1214ea7529e6130ae5bbd7f621f942fe36831fb3
Meher <gharsallahmeher1@gmail.com>
2023-03-20 14:24:15 +0100

    js basics : closures

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-/**
- * this keyword :
- * In JavaScript, the this keyword refers to an object.
+// /**
+//  * this keyword :
+//  * In JavaScript, the this keyword refers to an object.
 
-  Which object depends on how this is being invoked (used or called).
+//   Which object depends on how this is being invoked (used or called).
 
-  The this keyword refers to different objects depending on how it is used:
+//   The this keyword refers to different objects depending on how it is used:
 
-  *In an object method, this refers to the object.
-  *Alone, this refers to the global object.
-  *In a function, this refers to the global object.
-  *In a function, in strict mode, this is undefined.
-  *In an event, this refers to the element that received the event.
-*/
+//   *In an object method, this refers to the object.
+//   *Alone, this refers to the global object.
+//   *In a function, this refers to the global object.
+//   *In a function, in strict mode, this is undefined.
+//   *In an event, this refers to the element that received the event.
+// */
 
 class Person {
   constructor(name) {
@@ -25,15 +25,15 @@ class Person {
 }
 
 const person1 = new Person("meher");
-// console.log(person1);
-// person1.sayHello(); //'meher'
+console.log(person1);
+person1.sayHello(); //'meher'
 
-/**
- * whats a promise in jacasctipt
- *if the promise is fulfilled then it will return resolve
- *if the promise is failed it will return reject
- *with async we use try and catch like the exp in line 45
- */
+// /**
+//  * whats a promise in jacasctipt
+//  *if the promise is fulfilled then it will return resolve
+//  *if the promise is failed it will return reject
+//  *with async we use try and catch like the exp in line 45
+//  */
 
 const testPromise = new Promise((resolve, reject) => {
   resolve("ok");
@@ -51,13 +51,13 @@ async function main() {
   }
 }
 
-/**
- * mutability && immutability
- */
+// /**
+//  * mutability && immutability
+//  */
 
 const x = "meher";
 // x = "kejr"; //Uncaught TypeError: Assignment to constant variable.
-//we  cant reassign the value with const ==> immutable
+// //we  cant reassign the value with const ==> immutable
 let y = "immutable???";
 y = "mutable";
 console.log(y); //mutable , same with var
@@ -67,3 +67,38 @@ const obj = {
 };
 obj.name = "bel";
 console.log(obj); //obj keys is mutable
+
+/**
+ * closure
+ *in this case the count variable is a scoped variable , it can onnly be accessed 
+ inside the function counter() and by then we can access it inside the callback function
+ */
+
+function counter() {
+  let count = 0;
+  return () => {
+    return count++;
+  };
+}
+console.log(counter()); //output :  () => { return count++ }
+const c = counter();
+console.log(c());
+console.log(c());
+
+// // another exp
+
+function outerFunction(outVariable) {
+  return function innerFunction(insideVariable) {
+    console.log("outer variable :" + outVariable);
+    console.log("inside variable :" + insideVariable);
+  };
+}
+
+const newFunction = outerFunction("outside");
+newFunction("inside");
+//or we can access it this way
+outerFunction("outer")("inner");
+
+//simply we can say that every thing available in the outer function is available
+//inside the inner function since in javascript any thing in the inside has access to the outside
+//inner scope has access to the global scope , but if we reverse its not the case
